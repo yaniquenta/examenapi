a64a74ac661af2432c1a77982e24e4d16cb91fad
yaniquenta <152569380+yaniquenta@users.noreply.github.com>
2024-04-08 19:26:29 -0400

    Update rutaslibros.js

--- routes/rutaslibros.js
+++ routes/rutaslibros.js
@@ -58,63 +58,38 @@ rutas.delete('/eliminar/:id', async (req, res) =>{
     }
 });
 //// - Consultar la tarea mas reciente anadida a la base de datos
-rutas.get('/libro-reciente', async (req, res) =>{
+rutas.get('/Libro-reciente', async (req, res) =>{
     try {
         const tarea = await libroModel.findOne().sort({_id: -1});
-        res.json(libros);
+        res.json(tarea);
     }
     catch(error){
         res.status(404).json({mensaje: error.message});
     }
 });
 //// - Consultar una libro especifica por Id
-rutas.get('/libros-esp/:id', async (req, res) =>{
+rutas.post('/libros-esp/', async (req, res) =>{
     try {
-        console.log(req.params.id);
-        const tarea = await libroModel.findById(req.params.id);
+        const nuevaTarea = new libroModel({
+            NombreLibro : req.body.NombreLibro
+
+        });
+        const libros = await libroModel.find({ NombreLibro: req.body.NombreLibro });
         res.json(libros);
     }
     catch(error){
         res.status(404).json({mensaje: error.message});
     }
 });
-//// - Listar todas las libros por cantidad 5
-rutas.get('/tarea-prioridad/:id', async (req, res) =>{
+//// - Ordenar las tareas por prioridad de forma ascendente
+rutas.get('/ordenarCantidad', async (req, res) =>{
     try {
-        console.log(req.params.id);
-        const tareasPrioridad = await TareaModel.find({ prioridad: req.params.id});
-        res.json(tareasPrioridad);
+        const libros = await libroModel.find().sort({cantidad: -1});
+        res.json(libros);
     }
     catch(error){
         res.status(404).json({mensaje: error.message});
     }
 });
-
-////consultas ----------------------
-
-//
-//// - Ordenar las tareas por prioridad de forma ascendente
-//rutas.get('/ordenar-tarea', async (req, res) =>{
-//    try {
-//        const tareasASC = await TareaModel.find().sort({prioridad: 1});
-//        res.json(tareasASC);
-//    }
-//    catch(error){
-//        res.status(404).json({mensaje: error.message});
-//    }
-//});
-
-//// - Eliminar todas las tareas con una prioridad determinada
-//rutas.delete('/eliminar-prioridad/:prioridad', async (req, res) =>{
-//    try {
-//        console.log(req.params.prioridad);
-//        const prioridad = req.params.prioridad
-//        const eliminarTareas = await TareaModel.deleteMany({prioridad});
-//        res.json({mensaje: 'Tareas eliminada correctamente'});
-//        
-//    } catch(error){
-//        res.status(400).json({mensaje: error.message});
-//    }
-//});
 
 module.exports = rutas;
